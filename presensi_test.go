6245f56b8edf968e76d1d81940ae1181b2b4a6c0
Valen Rionald <vrionald@gmail.com>
2024-03-13 09:56:08 +0700

    Feat(Test): Test function get pukul 16:00

--- presensi_test.go
+++ presensi_test.go
@@ -25,5 +25,5 @@ func TestRekapBulanan(t *testing.T) {
 }
 
 func TestJamRamadhan(t *testing.T) {
-	fmt.Println("Waktu Ramadhan : ", GetJamRamadhan())
+	fmt.Println("Waktu Ramadhan : ", GetJamPulangRamadhan())
 }
