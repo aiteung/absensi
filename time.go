65359ba9f3789d4866510e473b063bbbcd26ab52
Valen Rionald <vrionald@gmail.com>
2024-03-13 09:06:07 +0700

    Ref(Code): Clear function get time now

--- time.go
+++ time.go
@@ -233,9 +233,3 @@ func GetMulaiPresensi() (waktumulai string) {
 	waktuMulai := time.Date(waktuSekarang.Year(), waktuSekarang.Month(), waktuSekarang.Day(), 6, 0, 0, 0, loc)
 	return waktuMulai.Format("15:04")
 }
-
-func GetTimeNow() time.Time {
-	loc, _ := time.LoadLocation("Asia/Jakarta")
-	waktuSekarang := time.Now().In(loc)
-	return waktuSekarang
-}
